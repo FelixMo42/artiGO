aab6c1b60bc9dda8c5cf6e86e4188392784b5351
FelixMo42 <felix@tacocat.com>
2018-06-26 10:28:50 -0700

    basic virtual bot movment simulator

--- main.py
+++ main.py
@@ -24,27 +24,32 @@ target = [900, 900]
 
 draw(500 - 30,500 - 30,60,60)
 
+def dist(xi,yi,xii,yii):
+	sq1 = (xi-xii)*(xi-xii)
+	sq2 = (yi-yii)*(yi-yii)
+	return math.sqrt(sq1 + sq2)
+
 def raycast(sx,sy,a):
 	if a == 180:
 		x = sx
 		for y in range(sy, 0, -1):
 			if map[x, y] == 1:
-				return (x, y)
+				return dist(sx, sy, x, y)
 	elif a > 0:
 		for x in range(0, width - sx):
 			y = sx + math.floor(x * math.tan(math.radians(a)))
 			if map[x, y] == 1:
-				return (x, y)
+				return dist(sx, sy, x, y)
 	elif a < 0:
 		for x in range(0, sx):
 			y = sx - math.floor(math.abs(x) * math.tan(math.radians(a)))
 			if map[x, y] == 1:
-				return (x, y)
+				return dist(sx, sy, x, y)
 	elif a == 0:
 		x = sx
 		for y in range(sy, height):
 			if map[x, y] == 1:
-				return (x, y)
+				return dist(sx, sy, x, y)
 
 def update():
 	ultra_LS = raycast(bot.pos[0], bot.pos[0], bot.angle + 45)
@@ -62,4 +67,15 @@ def update():
 
 	## end nutal magic ##
 
-	pass
+	LS = servo_FL + servo_BL
+	RS = serbo_FR + servo_BR
+
+	if LS > RS:
+		bot.angle += 0
+	elif RS > LS:
+		bot.angle -= 0
+
+	speed = (LS + RS) / 4
+
+	bot.pos[0] += speed * math.cos(math.radians(bot.angle))
+	bot.pos[1] += speed * math.sin(math.radians(bot.angle))

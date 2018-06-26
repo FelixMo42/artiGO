5d88d51b0cdc8cce4c34b0d946afb7240b371d3e
FelixMo42 <felix@tacocat.com>
2018-06-25 21:42:07 -0700

    add ray cast and start of update func

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def draw(sx,sy,w,h):
 
 bot = {
 	"pos" : [0, 0],
-	"size" : [20, 60]
+	"size" : [20, 60],
+	"angle" : 0
 }
 
 width = 1000
@@ -27,22 +28,38 @@ def raycast(sx,sy,a):
 	if a == 180:
 		x = sx
 		for y in range(sy, 0, -1):
-			if map[x][y] == 1:
+			if map[x, y] == 1:
 				return (x, y)
 	elif a > 0:
 		for x in range(0, width - sx):
-			y = sx + math.floor(x * 1)
-			if map[x][y] == 1:
+			y = sx + math.floor(x * math.tan(math.radians(a)))
+			if map[x, y] == 1:
 				return (x, y)
 	elif a < 0:
 		for x in range(0, sx):
-			y = sx - math.floor(x * 1)
-			if map[x][y] == 1:
+			y = sx - math.floor(math.abs(x) * math.tan(math.radians(a)))
+			if map[x, y] == 1:
 				return (x, y)
 	elif a == 0:
 		x = sx
 		for y in range(sy, height):
-			if map[x][y] == 1:
+			if map[x, y] == 1:
 				return (x, y)
 
-print( raycast(0,0,12) )
+def update():
+	ultra_LS = raycast(bot.pos[0], bot.pos[0], bot.angle + 45)
+	ultra_LC = raycast(bot.pos[0], bot.pos[0], bot.angle + 10)
+	ultra_FC = raycast(bot.pos[0], bot.pos[0], bot.angle + 0)
+	ultra_RC = raycast(bot.pos[0], bot.pos[0], bot.angle - 10)
+	ultra_RS = raycast(bot.pos[0], bot.pos[0], bot.angle - 45)
+
+	## start nural magic ##
+
+	servo_FL = 1
+	servo_FR = 1
+	servo_BL = 1
+	servo_BR = 1
+
+	## end nutal magic ##
+
+	pass

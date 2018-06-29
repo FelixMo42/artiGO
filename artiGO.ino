c31f6ef1f0a75a5cbae4fea98ba49f273fc87d2f
FelixMo42 <felix@tacocat.com>
2018-06-28 20:31:24 -0700

    arduio code for all motors

--- artiGO.ino
+++ artiGO.ino
@@ -1,25 +1,39 @@
-int RM_CL = 10;
-int RM_CCL = 11;
-int RM_S = 6;
+int FR_M_CL = 13;
+int FR_M_CCL = 7;
+int FR_M_S = 9;
 
-int LM_CL = 8;
-int LM_CCL = 9;
-int LM_S = 5;
+int FL_M_CL = 12;
+int FL_M_CCL = A0;
+int FL_M_S = 11;
 
-int ECHO = 3;
-int TRIG = 13;
+int BR_M_CL = 8;
+int BR_M_CCL = 5;
+int BR_M_S = 3;
 
-int LED = 13;
+int BL_M_CL = 10;
+int BL_M_CCL = 4;
+int BL_M_S = 6;
+
+int TRIG = 2; //nessisary
+
+int BR_US = 9;
+int FR_US = 8;
+int FC_US = A5;
+int FL_US = A4;
+int BL_US = A5;
 
 int outputs[] = {
-    RM_CL, RM_CCL, RM_S,
-    LM_CL, LM_CCL, LM_S,
-    TRIG,
-    LED
+    FR_M_CL, FR_M_CCL, FR_M_S,
+    FL_M_CL, FL_M_CCL, FR_M_S,
+    
+    BR_M_CL, BR_M_CCL, BR_M_S,
+    BL_M_CL, BL_M_CCL, BR_M_S,
+    
+    TRIG
 };
 
 int inputs[] = {
-  ECHO
+  BR_US, FR_US, FC_US, FL_US, BL_US
 };
 
 void setup() {
@@ -31,24 +45,101 @@ void setup() {
   for (int port : inputs) {
     pinMode(port, INPUT);
   }
-
-  digitalWrite(RM_CL, HIGH);
 }
 
 int dist() {
   digitalWrite(TRIG, HIGH);
-  delayMicroseconds(100);
+  delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
-  return pulseIn(ECHO, HIGH);
+  return pulseIn(FC_US, HIGH);
+}
+
+void set(String m, int s) {
+  if (m.equals("FL")) {
+    analogWrite(FL_M_S, s);
+    
+    if (s > 0) {
+      digitalWrite(FL_M_CL, HIGH);
+      digitalWrite(FL_M_CCL, LOW);
+    } else if (s == 0) {
+      digitalWrite(FL_M_CL, LOW);
+      digitalWrite(FL_M_CCL, LOW);
+    } else if (s < 0) {
+      digitalWrite(FL_M_CL, LOW);
+      digitalWrite(FL_M_CCL, HIGH);
+    }
+  } else if (m.equals("FR")) {
+    analogWrite(FR_M_S, s);
+    
+    if (s > 0) {
+      digitalWrite(FR_M_CL, HIGH);
+      digitalWrite(FR_M_CCL, LOW);
+    } else if (s == 0) {
+      digitalWrite(FR_M_CL, LOW);
+      digitalWrite(FR_M_CCL, LOW);
+    } else if (s < 0) {
+      digitalWrite(FR_M_CL, LOW);
+      digitalWrite(FR_M_CCL, HIGH);
+    }
+  } else if (m.equals("BL")) {
+    analogWrite(BL_M_S, s);
+    
+    if (s > 0) {
+      digitalWrite(BL_M_CL, HIGH);
+      digitalWrite(BL_M_CCL, LOW);
+    } else if (s == 0) {
+      digitalWrite(BL_M_CL, LOW);
+      digitalWrite(BL_M_CCL, LOW);
+    } else if (s < 0) {
+      digitalWrite(BL_M_CL, LOW);
+      digitalWrite(BL_M_CCL, HIGH);
+    }
+  } else if (m.equals("BR")) {
+    analogWrite(BR_M_S, s);
+    
+    if (s > 0) {
+      digitalWrite(BR_M_CL, HIGH);
+      digitalWrite(BR_M_CCL, LOW);
+    } else if (s == 0) {
+      digitalWrite(BR_M_CL, LOW);
+      digitalWrite(BR_M_CCL, LOW);
+    } else if (s < 0) {
+      digitalWrite(BR_M_CL, LOW);
+      digitalWrite(BR_M_CCL, HIGH);
+    }
+  }
 }
 
 void loop() {
-  int d = dist();
-  //Serial.println(d);
-  //delay(500);
-  if (d > 100) {
-    analogWrite(RM_S, 100);
+  int d = 0;
+  for (int i = 0; i < 5; i++) {
+    int n = dist();
+    //Serial.println(n);
+    if (n > 100) {
+      d += n;
+    }
+    delay(15);
+  }
+  d /= 5;
+  
+  Serial.println(d);
+
+  //set("FL", 100);
+  //set("FR", 100);
+  //set("BL", 100);
+  //set("BR", 100);
+
+  if (d > 500) {
+    set("FL", 100);
+    set("FR", 100);
+    set("BL", 100);
+    set("BR", 100);
   } else {
-    analogWrite(RM_S, 0);
-  } 
+    set("FL", 0);
+    set("FR", 0);
+    set("BL", 0);
+    set("BR", 0);
+  }
+  
+  delay(500);
 }
